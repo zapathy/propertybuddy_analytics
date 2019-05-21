e351c629661284b16cfe83c516e5ab1da273a990
zapathy <apathy.zsombor@gmail.com>
2019-05-22 00:55:18 +0200

    fixed warnings

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask_restplus import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 @api.route('/hello')
 class HelloWorld(Resource):
-    def get(self):
-        return {'hello','world'}
+    @staticmethod
+    def get():
+        return ['hello', 'world']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
